44b84d6cd1046c47e954f5a99e69067779bc4a4e
Khadijat Oyeleye <kaydee98@hotmail.co.uk>
2021-11-12 21:32:14 +0000

    changes made based on feedback received

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 
-const navHamburgerMenu = document.getElementById('navHamburgerMenu');
-const navLinkContainer = document.getElementById('nav__link-container');
+const navHamburgerMenu = document.querySelector(".nav__hamburger-menu");
+const navLinkContainer = document.querySelector(".nav__link-container");
 
 navHamburgerMenu.addEventListener("click", () => {
   navLinkContainer.classList.toggle("show");
+});
+
+
+const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav__link");
+const navLogo = document.querySelector(".nav__logo");
+const navHamburgerMenuBars = document.querySelector(".nav__hamburger-menu-bars")
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 980) {
+    nav.classList.add("scroll");
+    navLinks.forEach((navLink) => {
+      navLink.classList.add("scroll");
+    })
+    navLogo.classList.add("scroll");
+    navHamburgerMenuBars.classList.add("scroll");
+  } 
+  else {
+    nav.classList.remove("scroll");
+    navLinks.forEach((navLink) => {
+      navLink.classList.remove("scroll");
+    })
+    navLogo.classList.remove("scroll");
+    navHamburgerMenuBars.classList.remove("scroll");
+  }
 });
